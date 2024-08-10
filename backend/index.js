84fcc2c3c084ed73c35ad28f40a14f7f7df20d0d
Ali <2153970@cognizant.com>
2024-08-10 11:44:17 +0530

    middleware commit

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,15 @@
 const express = require('express');
-const mainRouter = require('./routes/index');
+const cors = require('cors');
 
+app.use(cors());
+app.use(express.json());
+const mainRouter = require('./routes/index');
 
 const app = express();
 
-app.use("/api/vi", mainRouter)
+
+app.use("/api/vi", mainRouter);
+
+app.listen(3000);
+
+
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,4 +1,8 @@
-const express=require('express');
-const router=express.Router();
+const express = require('express');
+const userRouter=require('./user');
+const router = express.Router();
 
-module.exports=router;
+router.use('/user',userRouter);
+
+
+module.exports = router;
