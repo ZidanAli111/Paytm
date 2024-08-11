70730540268e73a313a5e4481d96503404bb2570
Ali <2153970@cognizant.com>
2024-08-11 20:01:18 +0530

    auth middleware commit

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -5,12 +5,14 @@ const jwt = require('jsonwebtoken');
 const JWT_SECRET = require("../config"); x
 const router = express.Router();
 
+
 const signUpSchema = zod.object({
     username: zod.string().email(),
     password: zod.string(),
     firstName: zod.string(),
     lastName: zod.string()
 })
+
 router.post("/signup", async (req, res) => {
     const body = req.body;
     const { success } = signUpSchema.safeParse(req.body);
